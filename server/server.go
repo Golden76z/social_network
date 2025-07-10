fc6798f07c5e1aeceaf4d1a06776d2e44fd09d0d
Golden76z <damienprouet76@gmail.com>
2025-07-10 14:02:52 +0200

    Websockets adapted to GroupChat functionnalities

--- server/server.go
+++ server/server.go
@@ -22,10 +22,11 @@ import (
 
 func main() {
 	// Load configuration
-	cfg, err := config.Load()
+	err := config.Load()
 	if err != nil {
 		log.Fatalf("Failed to load config: %v", err)
 	}
+	cfg := config.GetConfig()
 
 	// Run DB migrations
 	if err := migrations.RunMigrations(cfg.DBPath, cfg.MigrationsDir); err != nil {
@@ -43,8 +44,8 @@ func main() {
 	go utils.StartSessionCleanup(dbService.DB, 1*time.Hour)
 
 	// Initialize WebSocket hub
-	wsHub := websockets.NewHub(dbService.DB)
-	go wsHub.Run()
+	websockets.InitHub(dbService.DB)
+	wsHub := websockets.GetHub()
 
 	// Setup router and middleware
 	r := routes.New()
@@ -54,7 +55,7 @@ func main() {
 	r.Use(middleware.SetupCORS())
 
 	// Setup routes
-	routes.SetupRoutes(r, dbService.DB, wsHub)
+	routes.SetupRoutes(r, dbService.DB, wsHub, cfg)
 
 	// Start server
 	startServer(r, cfg)
