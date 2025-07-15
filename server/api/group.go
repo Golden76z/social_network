469cc0c6dbea49b1bf8c750d12f631677240b930
Golden76z <damienprouet76@gmail.com>
2025-07-15 17:08:56 +0200

    Group Handler creation added - Retrieving the Token and making the Postman request

--- server/api/group.go
+++ server/api/group.go
@@ -1,9 +1,26 @@
 package api
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Golden76z/social-network/config"
+	"github.com/Golden76z/social-network/utils"
+)
 
 func CreateGroupHandler(w http.ResponseWriter, r *http.Request) {
-
+	fmt.Println("test")
+	token, err := r.Cookie("jwt_token")
+	if err != nil {
+		http.Error(w, "Unauthorized: Missing token", http.StatusUnauthorized)
+		return
+	}
+	key := config.Config.JWTKey
+	claims, errValidation := utils.ValidateToken(token.Value, key)
+	if errValidation != nil {
+		fmt.Println("Error decoding token")
+	}
+	fmt.Println(claims)
 }
 
 func GetGroupHandler(w http.ResponseWriter, r *http.Request) {
