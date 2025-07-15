5131ffc1bb511d15027258ac7466a021c3d02f92
Golden76z <damienprouet76@gmail.com>
2025-07-15 17:08:56 +0200

    Group Handler creation added - Retrieving the Token and making the Postman request

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -26,7 +26,7 @@ func setupProtectedRoutes(r *Router) {
 	r.Group(func(r *Router) {
 		// Apply auth middleware
 		r.Use(middleware.AuthMiddleware())
-		r.Use(middleware.CSRFMiddleware)
+		// r.Use(middleware.CSRFMiddleware)
 		r.Use(middleware.RateLimit(100, time.Minute))
 
 		setupUserRoutes(r)
