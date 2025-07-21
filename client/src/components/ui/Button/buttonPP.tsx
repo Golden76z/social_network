c77eaffdb0a69b8c839bc4c6cbf35085c4824c49
avalaune <alex.valauney01@gmail.com>
2025-07-21 15:53:18 +0200

    comment navBar part 2

--- client/src/components/ui/Button/buttonPP.tsx
+++ client/src/components/ui/Button/buttonPP.tsx
@@ -4,41 +4,37 @@ import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 
-const buttonAcceptVariants = cva(
-    "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+
+const buttonPPVariants = cva(
+    "inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
     {
         variants: {
             variant: {
-                PrimaryRest:
-                    "bg-chart-1 text-white shadow-xs hover:bg-chart-2",
-                PrimaryDisabled:
-                    "bg-chart-2 text-white shadow-xs",
-                SecondaryRest:
-                    "rounded-[5px] border border-chart-1 text-chart-1 shadow-xs hover:border-chart-2 hover:text-chart-2",
-                SecondaryDisabled:
-                    "rounded-[5px] border border-chart-2 text-chart-2 shadow-xs",
+                PP:
+                    "bg-[url('https://tse2.mm.bing.net/th/id/OIP.vg1KOuYtiZI2V0_ZXB-05gHaGc?r=0&pid=Api')] bg-cover bg-center text-transparent hover:shadow-xs",
+                Login:
+                    "text-chart-1 border border-chart-1 hover:border-chart-2 hover:text-chart-2",
             },
             size: {
-                size1: "h-[40px] w-[75px]",
-                size2: "h-[40px] w-[105px]",
-                size3: "h-[40px] w-[190px]",
+                size1: "h-[35px] w-[35px]",
+                size2: "h-[20px] w-[105px]",
             },
         },
         defaultVariants: {
-            variant: "PrimaryRest",
+            variant: "PP",
             size: "size1",
         },
     }
 )
 
-function ButtonAccept({
+function ButtonPP({
     className,
     variant,
     size,
     asChild = false,
     ...props
 }: React.ComponentProps<"button"> &
-    VariantProps<typeof buttonAcceptVariants> & {
+    VariantProps<typeof buttonPPVariants> & {
         asChild?: boolean
     }) {
     const Comp = asChild ? Slot : "button"
@@ -46,10 +42,14 @@ function ButtonAccept({
     return (
         <Comp
             data-slot="button"
-            className={cn(buttonAcceptVariants({ variant, size, className }))}
+            className={cn(buttonPPVariants({ variant, size, className }))}
             {...props}
-        />
+        >
+            {variant === "Login" && (
+                <text>Login/Register</text>
+            )}
+        </Comp>
     )
 }
 
-export { ButtonAccept, buttonAcceptVariants }
+export { ButtonPP, buttonPPVariants }
