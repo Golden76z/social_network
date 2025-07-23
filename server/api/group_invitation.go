1b83f747b23797dae22c63c2fe1fa1a02baeb71e
Golden76z <damienprouet76@gmail.com>
2025-07-23 16:41:27 +0200

    CRUD Methods and Handlers done for the Group Members - Tested on Postman - CSV Fixed for group membership

---
+++ server/api/group_invitation.go
@@ -0,0 +1,19 @@
+package api
+
+import "net/http"
+
+func CreateGroupInvitationHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetGroupInvitationHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UpdateGroupInvitationHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteGroupInvitationHandler(w http.ResponseWriter, r *http.Request) {
+
+}
