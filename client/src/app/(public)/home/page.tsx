d30709845be280882873f74a2ac7d709eeafb745
acasimir <alexandre.casimir974@gmail.com>
2025-07-25 11:28:02 +0200

    debut link front to back

--- client/src/app/(public)/home/page.tsx
+++ client/src/app/(public)/home/page.tsx
@@ -12,17 +12,17 @@ const HomePage: React.FC = () => {
   const { user } = useAuth();
 
   return (
-    <div className="min-h-screen bg-gray-50 pb-16 md:pb-0"> {/* extra padding bottom for mobile nav */}
+    <div className="min-h-screen bg-gray-50 pb-16 md:pb-0">
+      {' '}
+      {/* extra padding bottom for mobile nav */}
       {/* Navbar */}
       <Header />
-
       {/* Main Content */}
       <div className="flex flex-col md:flex-row max-w-full mx-auto">
         {/* Left Sidebar - desktop only */}
         <div className="hidden md:block w-[20%] min-h-screen bg-white border-r border-gray-200 p-4">
           <SideBarLeft variant="sidebar" />
         </div>
-
 
         {/* Main Feed - full width on mobile */}
         <div className="w-full md:w-[70%] px-4 md:px-8 py-6">
@@ -59,7 +59,6 @@ const HomePage: React.FC = () => {
           <SideBarRight />
         </div>
       </div>
-
       {/* Bottom Navigation - mobile only */}
       <div className="fixed bottom-0 left-0 right-0 z-50 md:hidden border-t border-gray-200 bg-white shadow-sm">
         <SideBarLeft variant="bottom" />
